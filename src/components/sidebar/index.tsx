07a219bd5b5ed7fde509735eb0609712be12904e
Эдгар Оганнисян <ESOgannisyan@sberbank.ru>
2023-09-29 17:07:53 +0300

    add sidebar and styles

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,6 +1,25 @@
-import cls from 'classnames';
-import styles from './sidebar.module.css';
+import cls from "classnames";
+import styles from "./sidebar.module.css";
+import MainImg from "../../assets/images/MainImg.jpg";
 
 export const SideBar = () => {
-  return <div className={cls(styles.sidebar)}></div>;
+  return (
+    <div className={cls(styles.sidebar)}>
+      <div className={cls(styles.sidebar_mainimg)}>
+        <img
+          src={MainImg}
+          alt="Main"
+          className={cls(styles.sidebar_mainimg_img)}
+        />
+        <div className={cls(styles.sidebar_mainimg_credentials)}>
+          <span className={cls(styles.sidebar_mainimg_credentials_name)}>
+            Edgar Hovhannisyan
+          </span>
+          <span className={cls(styles.sidebar_mainimg_credentials_position)}>
+            Full stack developer
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
