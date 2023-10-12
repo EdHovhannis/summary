33c89c8d533696aa79ec1cc79612c421c3a210fe
Edgar <ed.hovhannis1992@gmail.com>
2023-10-12 22:07:08 +0300

    add some experiancies to state

--- src/components/content/index.tsx
+++ src/components/content/index.tsx
@@ -1,92 +1,82 @@
 import cls from 'classnames';
 import styles from './content.module.css';
+import { experiences } from '../../state/state';
 
 export const Content = () => {
+  const getJobStatus = (status: boolean) => {
+    if (status) {
+      return 'CURRENT';
+    }
+    return '';
+  };
   return (
     <div className={cls(styles.content)}>
-      <div className={cls(styles.content_experiences, styles.work_experiences)}>
-        <div className={cls(styles.work_experiences_header)}>
-          <i className={'fa-solid fa-suitcase'}></i>
-          <span className={cls(styles.work_experiences_header_text)}>
-            Work Experience
-          </span>
-        </div>
-        <div className={cls(styles.content_experiences_jobs)}>
-          <div className={cls(styles.content_experiences_jobs_info)}>
-            <h4 className={cls(styles.content_experiences_jobs_info_header)}>
-              Front End developer / SberTech
-            </h4>
-            <div>
-              <i
-                className={cls(
-                  'fa-solid fa-calendar-days',
-                  styles.content_experiences_jobs_info_icon
-                )}
-              ></i>
-              <span
-                className={cls(styles.content_experiences_jobs_info_datefrom)}
-              >
-                &nbsp;Jan 2015&nbsp;&#8212;&nbsp;
+      {experiences.map((experience, i) => (
+        <div
+          key={experience.id}
+          className={cls(styles.content_experiences_wrapper)}
+        >
+          <div
+            className={cls(
+              styles.content_experiences,
+              styles[`${experience.grid}_experiences`]
+            )}
+          >
+            <div className={cls(styles.work_experiences_header)}>
+              <i className={experience.icon}></i>
+              <span className={cls(styles.work_experiences_header_text)}>
+                {experience.title}
               </span>
-              <span
-                className={cls(styles.content_experiences_jobs_info_current)}
-              >
-                &nbsp;CURRENT&nbsp;
-              </span>
-              <p className={cls(styles.content_experiences_jobs_info_text)}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
-                repellendus iste accusantium molestias et ut quibusdam rem
-                temporibus quasi exercitationem, nam deserunt officia debitis.
-                Obcaecati id alias blanditiis nobis accusamus.
-              </p>
+            </div>
+            <div className={cls(styles.content_experiences_jobs)}>
+              {(
+                experience.work ||
+                experience.education ||
+                experience.other
+              ).map((work) => (
+                <div
+                  key={work.id}
+                  className={cls(styles.content_experiences_jobs_info)}
+                >
+                  <h4
+                    className={cls(styles.content_experiences_jobs_info_header)}
+                  >
+                    {work.position}
+                  </h4>
+                  <div>
+                    <i
+                      className={cls(
+                        work.calendarIcon,
+                        styles.content_experiences_jobs_info_icon
+                      )}
+                    ></i>
+                    <span
+                      className={cls(
+                        styles.content_experiences_jobs_info_datefrom
+                      )}
+                    >
+                      {work.startedAt}&nbsp;&#8212;&nbsp;
+                    </span>
+                    <span
+                      className={cls(
+                        styles.content_experiences_jobs_info_current
+                      )}
+                    >
+                      {getJobStatus(work.currentJob)}
+                    </span>
+                    <pre
+                      className={cls(styles.content_experiences_jobs_info_text)}
+                    >
+                      {work.description}
+                    </pre>
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
-          <div className={cls(styles.content_experiences_jobs_info)}>
-            <h4 className={cls(styles.content_experiences_jobs_info_header)}>
-              Front End developer / SberTech
-            </h4>
-            <div>
-              <i
-                className={cls(
-                  'fa-solid fa-calendar-days',
-                  styles.content_experiences_jobs_info_icon
-                )}
-              ></i>
-              <span
-                className={cls(styles.content_experiences_jobs_info_datefrom)}
-              >
-                &nbsp;Jan 2015&nbsp;&#8212;&nbsp;
-              </span>
-              <span
-                className={cls(styles.content_experiences_jobs_info_current)}
-              >
-                &nbsp;CURRENT&nbsp;
-              </span>
-              <p className={cls(styles.content_experiences_jobs_info_text)}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
-                repellendus iste accusantium molestias et ut quibusdam rem
-                temporibus quasi exercitationem, nam deserunt officia debitis.
-                Obcaecati id alias blanditiis nobis accusamus.
-              </p>
-            </div>
-          </div>
-          {/* <div>job-2</div>
-          <div>job-3</div> */}
+          <div className="gradient">test</div>
         </div>
-      </div>
-      <div
-        className={cls(
-          styles.content_experiences,
-          styles.education_experiences
-        )}
-      >
-        Hello
-      </div>
-      <div
-        className={cls(styles.content_experiences, styles.other_experiences)}
-      >
-        Hello
-      </div>
+      ))}
     </div>
   );
 };
